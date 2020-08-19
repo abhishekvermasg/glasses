b7c233e6ff67e218123f2ca00762ad542346c8b4
Francesco <zuppif@usi.ch>
2020-08-19 19:58:38 +0200

    as

--- test/test_Inception.py
+++ test/test_Inception.py
@@ -4,6 +4,6 @@ from glasses.nn.models.classification.inception import Inception
 
 def test_Inception():
     x = torch.rand(1, 3, 224, 224)
-    model = Inception().eval()
+    # model = Inception().eval()
     pred = model(x)
     assert pred.shape[-1] == 1000
